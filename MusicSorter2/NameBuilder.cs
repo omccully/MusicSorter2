4613378481692731114c3601d6b4e6dd52de52cb
FootKjott <oscarmccully@gmail.com>
2017-07-22 22:47:16 -0400

    .net framework 4.0, refactor NameBuilder with LINQ+regex

--- MusicSorter2/NameBuilder.cs
+++ MusicSorter2/NameBuilder.cs
@@ -1,93 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MusicSorter2
 {
+    /// <summary>
+    /// This class is used to improve the efficiency of determining the new name for the music files.
+    /// A NameBuilder object with a certain format needs to be constructed once, and subsequent calls to
+    /// the Build method are much faster than it would be to use the Format string each time. 
+    /// </summary>
     class NameBuilder
     {
-        int[] Tags { get; set; }
+        class SongAttribute
+        {
+            public string AttributeName { get; private set; }
+            public string AttributeTag { get; private set; }
+
+            public SongAttribute(string AttributeName, string AttributeTag)
+            {
+                this.AttributeName = AttributeName;
+                this.AttributeTag = AttributeTag;
+            }
+        }
+
+        static class SongAttributeCollection
+        {
+            public static readonly SongAttribute[] SongAttributes = {
+                new SongAttribute("TRACK_NUMBER", "#"),
+                new SongAttribute("SONG_TITLE", "T"),
+                new SongAttribute("ALBUM", "AL"),
+                new SongAttribute("ARTIST", "AR")
+            };
+
+            public static int IndexOf(SongAttribute sa_needle)
+            {
+                for(int i = 0; i < SongAttributes.Length; i++)
+                {
+                    if (SongAttributes[i] == sa_needle) return i;
+                }
+                throw new Exception("sa_needle not in SongAttributeCollection");
+            }
+
+            public static SongAttribute SongAttributeFromTag(string tag)
+            {
+                tag = tag.Replace("{", "").Replace("}", "");
+                foreach(SongAttribute sa in SongAttributes)
+                {
+                    if(sa.AttributeTag == tag) return sa;
+                }
+                throw new Exception("Invalid tag");
+            }
+
+            /// <summary>
+            /// Array of song attribute tags
+            /// </summary>
+            public static string[] Tags
+            {
+                get
+                {
+                    return SongAttributes.Select(attr => attr.AttributeTag).ToArray();
+                }
+            }
+
+            /// <summary>
+            /// Regex to match any of the tags (wrapped in curly braces)
+            /// </summary>
+            public static Regex TagsRegex
+            {
+                get
+                {
+                    // map SongAttributeTags so elements have escaped curly braces
+                    IEnumerable<string> tagged = Tags.Select(s => "\\{" + s + "\\}");
+
+                    // regex OR between tags
+                    string regex = String.Join("|", tagged);
+
+                    return new Regex(regex);
+                }
+            }
+        }
+
+        /* Tags/Text - Variables that express the file name format
+         * Invariant: Tags.Count + 1 == Text.Count
+         * The resultant file names are built like this using the values passed to Build:
+         * Text[0] + valueof(Tags[0]) + Text[1] + valueof(Tags[1]) ... valueof(Tags[n-1]) + Text[n]
+         */
+        SongAttribute[] Tags { get; set; }
         string[] Text { get; set; }
 
         public NameBuilder(string Format)
         {
-            List<int> Tags = new List<int>();
-            List<string> Text = new List<string>();
-            string CurrentTag = null;
-            string CurrentText = "";
-            int CurlyIndex = 0;
-            foreach (char c in Format)
-            {
-                if (c == '{')
-                {
-                    Text.Add(CurrentText);
-                    CurrentText = null;
-                    CurrentTag = "";
-                    CurlyIndex++;
-                    if (CurlyIndex == 2) throw new Exception("Invalid Tag: Embedded braces");
-                }
-                else if (c == '}')
-                {
-                    if (CurlyIndex == 0) throw new Exception("Invalid Tag: Embedded braces");
-                    switch (CurrentTag.ToUpper())
-                    {
-                        case "#":
-                            Tags.Add(0);
-                            break;
-                        case "T":
-                            Tags.Add(1);
-                            break;
-                        case "AL":
-                            Tags.Add(2);
-                            break;
-                        case "AR":
-                            Tags.Add(3);
-                            break;
-                        default:
-                            throw new Exception("Invalid Tag: '" + CurrentTag + "'");
-                    }
-                    CurrentText = "";
-                    CurrentTag = null;
-                    CurlyIndex--;
-                }
-                else
-                {
-                    switch (CurlyIndex)
-                    {
-                        case 0:
-                            CurrentText += c;
-                            break;
-                        case 1:
-                            CurrentTag += c;
-                            break;
-                    }
-                }
-            }
-            Text.Add(CurrentText);
-            this.Text = Text.ToArray();
-            this.Tags = Tags.ToArray();
+            Regex r = SongAttributeCollection.TagsRegex;
+
+            // Get array of non-tag text in Format
+            this.Text = r.Split(Format); 
+
+            // Get array of SongAttribute objects in order in which they appear in Format
+            this.Tags = r.Matches(Format).Cast<Match>().Select(
+                m => SongAttributeCollection.SongAttributeFromTag(m.Value)).ToArray();
         }
 
+        /// <summary>
+        /// Builds a song name with the format specified by the object and the arguments passed. 
+        /// </summary>
+        /// <param name="Number"></param>
+        /// <param name="Title"></param>
+        /// <param name="Album"></param>
+        /// <param name="Artist"></param>
+        /// <returns></returns>
         public string Build(string Number, string Title, string Album, string Artist)
         {
+            string[] attrib_vals = { Number, Title, Album, Artist };
+
             StringBuilder bob = new StringBuilder(Text[0]);
             for (int i = 0; i < Tags.Length; i++)
             {
-                switch (Tags[i])
+                for (int j = 0; j < SongAttributeCollection.SongAttributes.Length; j++)
                 {
-                    case 0:
-                        bob.Append(Number);
+                    if(Tags[i] == SongAttributeCollection.SongAttributes[j])
+                    {
+                        bob.Append(attrib_vals[j]);
                         break;
-                    case 1:
-                        bob.Append(Title);
-                        break;
-                    case 2:
-                        bob.Append(Album);
-                        break;
-                    case 3:
-                        bob.Append(Artist);
-                        break;
+                    }
                 }
+
                 bob.Append(Text[i + 1]);
             }
             return bob.ToString();
